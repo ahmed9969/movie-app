885da4b0643afd224e15283a67fe930940b98d0b
ahmed9969 <ahmedsaqib9969@gmail.com>
2026-03-13 23:29:33 +0500

    my first movie app

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+API_KEY = "2b054ec6549477dae6ef2db2bd55ad09"
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/results")
+def results():
+    movie = request.args.get("movie")
+    
+    url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&query={movie}"
+    response = requests.get(url)
+    data = response.json()
+    
+    movies = data["results"]
+    
+    return render_template("results.html", movie=movie, movies=movies)
+
+app.run()
